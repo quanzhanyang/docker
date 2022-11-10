b0fd94a61432d03fc8eb4c5038d47672c3011e66
MarkQuan <33057617@qq.com>
2022-11-10 23:14:31 +0800

    Initial commit

---
+++ src/main/java/com/quanstudent/dockerpeakage/DockerpeakageApplication.java
@@ -0,0 +1,22 @@
+package com.quanstudent.dockerpeakage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerpeakageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerpeakageApplication.class, args);
+    }
+
+
+    @GetMapping("/demo")
+    public String demoController() {
+        return "Hello";
+    }
+
+}
---
+++ src/test/java/com/quanstudent/dockerpeakage/DockerpeakageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quanstudent.dockerpeakage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerpeakageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
